faecd6f00d879b64c2a9be6222f6ad4037c49b66
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-23 14:18:35 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231223/데이터 교환/exchange-data.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5, b = 6, c = 7;
+    int temp1 = b;
+    int temp2 = c;
+    b = a;
+    c = temp1;
+    a = temp2;
+
+    cout << a << endl << b << endl << c;
+    return 0;
+}
