4c1155c5daad1619bb89ac85bb79402a5ba2ccfa
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-24 15:22:42 +0900

    Title: 두수의 곱과 몫, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231224/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
+++ 231224/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -6,6 +6,6 @@ int main() {
     cin >> a >> b;
     
     cout << a << " * " << b << " = " << a * b << endl;
-    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " / " << b << " = " << a / b
     return 0;
 }
