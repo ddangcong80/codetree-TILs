bc1e1e560994bc19966d440278e8dc91fff38f9d
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-24 20:42:57 +0900

    Title: 코로나 메뉴얼, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231224/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a, b, c;
+    int d, e, f, count;
+    cin >> a >> d >> b >> e >> c >> f;
+
+    if((a == 'Y') && (d >= 37)) {
+        count += 1;
+    }
+    if((b == 'Y') && (e >= 37)) {
+        count += 1;
+    }
+    if((c == 'Y') && (f >= 37)) {
+        count += 1;
+    }
+
+    if(count >= 2) {
+        cout << "E";
+    }
+    else {
+        cout << "N";
+    }
+    return 0;
+}
