33424d24b76ef50a9d252757103d487f20588ca2
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-24 15:21:10 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231224/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int wei, hei;
+    cin >> wei >> hei;
+    wei += 8;
+    hei *= 3;
+    cout << wei << endl;
+    cout << hei << endl;
+    cout << wei * hei;
+    return 0;
+}
