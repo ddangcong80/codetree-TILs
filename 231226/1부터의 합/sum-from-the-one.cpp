ec0e4aca52d97e76fbfaa1786c7e31a7b1dd7fed
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-26 13:04:04 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231226/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    for(int i = 1; i <= 100; i++) {
+        if((sum + i) > n) {
+            cout << i;
+            break;
+        }
+        sum += i;
+    }
+    return 0;
+}
