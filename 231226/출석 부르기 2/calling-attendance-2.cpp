1fa281c11289dc4d59c0a1d38a782b0a5bf5eba0
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-26 13:18:09 +0900

    Title: 출석 부르기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    while(true) {
+        cin >> num;
+        if(num == 1) {
+            cout << "John" << endl;
+        }
+        else if(num == 2) {
+            cout << "Tom" << endl;
+        }
+        else if(num == 3) {
+            cout << "Paul" << endl;
+        }
+        else if(num == 4) {
+            cout << "Sam" << endl;
+        }
+        else {
+            cout << "Vacancy";
+            break;
+        }
+    }
+    return 0;
+}
