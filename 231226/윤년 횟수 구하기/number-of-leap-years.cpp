7c4099cbfee5bf08a77a7f3c11e120180d3ba07c
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-26 12:30:13 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 4 == 0) {
+            if((i % 100 == 0) && (i % 400 != 0)) {
+            }
+            else {
+                cnt++;
+            }
+        }
+        else {
+        }
+    }
+    cout << cnt;
+    return 0;
+}
