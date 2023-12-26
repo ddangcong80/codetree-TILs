fa6b68308bc902608c0ffe2884d6eef1b76ff36a
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-26 20:38:14 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    bool check = true;
+    cin >> a >> b >> c;
+
+    for(int i = a; i <= b; i++) {
+        if(i % c == 0) {
+            check = false;
+        }
+    }
+    if(check) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
