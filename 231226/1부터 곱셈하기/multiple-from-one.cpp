a5b3a7eb474c4df588a84fa9c94f193f56d7b8ba
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-26 13:06:22 +0900

    Title: 1부터 곱셈하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231226/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, prod = 1;
+    cin >> n;
+    for(int i = 1; i < 10; i++) {
+        if((i * prod) >= n) {
+            cout << i;
+            break;
+        }
+        prod *= i;
+    }
+    return 0;
+}
