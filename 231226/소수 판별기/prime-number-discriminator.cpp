a7857bcebefd6e2b1f75e9df76b55e7a6f281b0c
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-26 20:39:44 +0900

    Title: 소수 판별기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    bool check = true;
+    cin >> n;
+
+    for(int i = 2; i < n; i++) {
+        if(n % i == 0) {
+            check = false;
+        }
+    }
+    if(check) {
+        cout << "P";
+    }
+    else {
+        cout << "C";
+    }
+    return 0;
+}
