5d1c1af2509bee4ed726fb55ea57092a6ba30e18
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2023-12-28 22:20:05 +0900

    Title: 별표 출력하기 5, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231228/별표 출력하기 5/print-star-5.cpp
+++ 231228/별표 출력하기 5/print-star-5.cpp
@@ -6,8 +6,7 @@ int main() {
     cin >> n;
     for(int i = 0; i < n; i++) {
         for(int j = n - i; j > 0; j--) {
-            for(int k = 0; k < n-i;) {
-                k++;
+            for(int k = 0; k < n-i; k++) {
                 cout << "*";
             }
             cout << " ";
