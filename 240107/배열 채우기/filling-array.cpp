f9cfe37a9b5925db967befe51c40bf762d32a7f5
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2024-01-07 14:38:30 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240107/배열 채우기/filling-array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int cnt = 0;
+
+    for(int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if(arr[i] == 0) {
+            break;
+        }
+        cnt++;
+    }
+
+    for(int i = 0; i < cnt; i++) {
+        cout << arr[cnt - i - 1] << " ";
+    }
+    return 0;
+}
