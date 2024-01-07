99221861de2d2cc4805b4508951f2b9e0598a9dc
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2024-01-07 13:17:37 +0900

    Title: 별표 출력하기 10, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240107/별표 출력하기 10/print-star-10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n * 2 ; i++) {
+        if(i % 2 == 0) {
+            for(int j = 0; j < i / 2 + 1; j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+        else {
+            for(int k = 0; k < n - (i / 2); k++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
