f6670692bf1a0f0f1a0b86023a9f2af3fc28ea53
ddangcong80 <154437678+ddangcong80@users.noreply.github.com>
2024-01-07 16:06:14 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240107/특정 조건에 맞게 출력하기/print-in-specific-conditions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100];
+
+    for(int i = 0; i < 100; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < 100; i++) {
+        if(arr[i] == 0) {
+            break;
+        }
+        if(arr[i] % 2 == 1) {
+            cout << arr[i] + 3 << " ";
+        }
+        else {
+            cout << arr[i] / 2 << " ";
+        }
+    }
+    return 0;
+}
